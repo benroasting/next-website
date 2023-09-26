b928d0985e9be698c6e60b8f783ee9bdafd71c84
Ben Young <developing.ben@gmail.com>
2023-09-26 14:53:17 -0500

    added a couple things

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,25 +1,25 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Navbar() {
   return (
     <div>
-        <ul className='flex justify-center'>
-            <li className='p-3'>
-                <Link href="/">Home</Link>
-            </li>
-            <li className='p-3'>
-                <Link href="/about">About</Link>
-            </li>
-            <li className='p-3'>
-                <Link href="/projects">Projects</Link>
-            </li>
-            <li className='p-3'>
-                <Link href="/contact">Contact</Link>
-            </li>
-        </ul>
+      <ul className="flex justify-center">
+        <li className="p-3">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="p-3">
+          <Link href="/about">About</Link>
+        </li>
+        <li className="p-3">
+          <Link href="/projects">Projects</Link>
+        </li>
+        <li className="p-3">
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,48 @@
-import { Inter } from 'next/font/google'
+import { TypeAnimation } from "react-type-animation";
+import { Overpass_Mono } from "next/font/google";
+import Image from "next/image";
+import benImage from "../public/ben-image.png";
 
-const inter = Inter({ subsets: ['latin'] })
+const overpass = Overpass_Mono({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex  min-h-screen flex-col items-center p-24 font ${overpass.className}`}
     >
-      Main Page
+      <div className="border-b-2 border-sky-600 p-5">
+        <Image
+          className="drop-shadow-lg rounded-full filter grayscale"
+          height={250}
+          width={250}
+          src={benImage}
+          alt=""
+        />
+      </div>
+
+      <div className="p-5">
+        <TypeAnimation
+          sequence={[
+            // Same substring at the start will only be typed out once, initially
+            "Hi, My name is Ben Young",
+            2000, // wait 1s before replacing "Mice" with "Hamsters"
+            "I am a Software Engineer.",
+            2000,
+            "I want to improve the lives of others through my actions and my code.",
+            2000,
+          ]}
+          wrapper="span"
+          cursor="true"
+          className={overpass.className}
+          speed={50}
+          style={{
+            fontSize: "2em",
+            color: "black",
+            display: "inline-block",
+          }}
+          repeat={Infinity}
+        />
+      </div>
     </main>
-  )
+  );
 }
