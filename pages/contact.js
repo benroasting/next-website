858c96772951b97e1cda2c8470588eaabe242080
Ben Young <developing.ben@gmail.com>
2023-09-27 14:56:31 -0500

    added contact info -- still needs styling

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 function contact() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-between p-24'>contact</div>
-  )
+    <div className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-4xl font-bold">Contact:</h1>
+      <div className="flex">
+        <div>
+          <p className="text-xl">Ben Young</p>
+          <p className="text-lg">Software Engineer & Reformed Coffee Snob</p>
+        </div>
+        <div>
+          <div className="py-2">
+            <p className="text-lg">developing.ben@gmail.com</p>
+            <p className="text-lg">501.548.5087</p>
+            <p className="text-lg">currently in Nashville, TN</p>
+          </div>
+
+          <p className="text-lg">
+            <Link href={"https://github.com/benroasting"}>GitHub</Link>
+          </p>
+          <p className="text-lg">
+            <Link href={"https://linkedin.com/in/benjamin-young-developing"}>
+              LinkedIn
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default contact
+export default contact;
--- pages/projects.js
+++ pages/projects.js
@@ -1,49 +1,61 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import projectsMap from './components/projectsMap'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import projectsMap from "./components/projectsMap";
 
 function projects() {
   return (
-    <div className='flex min-h-screen flex-col items-center justify-between p-24'>
-        <div className='flex p-5'>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex p-5">
         {projectsMap.map((project) => (
-        
-            <div key={project.title} className="w-1/4 rounded-lg shadow-xl bg-white m-5">
-                <img
-                    src={project.src}
-                    alt=""
-                    className="rounded-t-lg h-60 w-full object-cover"
-                />
-            <div className="p-6">
-              <h5
-                className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+          <div
+            key={project.title}
+            className="w-[20rem] rounded-lg shadow-xl bg-white m-5"
+          >
+            <img
+              src={project.src}
+              alt=""
+              className="rounded-t-lg h-60 w-full object-cover"
+            />
+            <div className="px-6 pt-6 items-center">
+              <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
                 {project.title}
               </h5>
-              <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-              {project.description}
+              <p className="mb-2 text-sm text-neutral-600 dark:text-neutral-200">
+                {project.description}
               </p>
-              <div className='flex justify-between '> 
-                <Link href={project.code}
+              {project.stack.map((tech) => (
+                <span
+                  key={project.stack[tech]}
+                  className="m-1 px-2 text-sm bg-slate-400 rounded-lg text-white uppercase justify-center"
+                >
+                  {tech}
+                </span>
+              ))}
+              <div className="flex justify-between ">
+                <Link
+                  href={project.code}
                   type="button"
-                  className="inline-block text-xs font-medium uppercase text-black">
+                  className="inline-block text-xs font-medium uppercase text-black"
+                >
                   Code
-                </Link >
-                {project.website? 
-                <Link href={project.website}
-                  type="button"
-                  className="inline-block text-xs font-medium uppercase text-black">
-                  View Live
-                </Link > : null }
+                </Link>
+                {project.website ? (
+                  <Link
+                    href={project.website}
+                    type="button"
+                    className="inline-block text-xs font-medium uppercase text-black"
+                  >
+                    View Live
+                  </Link>
+                ) : null}
               </div>
             </div>
           </div>
-                  
         ))}
-        </div>
       </div>
-
-  )
+    </div>
+  );
 }
 
-export default projects
+export default projects;
